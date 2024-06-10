4c6d37baa62c0e0308b0c059fd15e86b66419acc
oomori-you <valsamina@gmail.com>
2024-06-10 19:36:51 +0900

    feat: 123

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,3 +28,15 @@ Route::get('/tweet/update/{tweetId}', App\Http\Controllers\Tweet\Update\IndexCon
 Route::put('/tweet/update/{tweetId}', App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put')->where('tweetId', '[0-9]+');
 
 Route::delete('/tweet/delete/{tweetId}', App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
