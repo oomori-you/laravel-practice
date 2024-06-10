a2b86e2829879b3c13321f4be6f77834cb537299
oomori-you <valsamina@gmail.com>
2024-06-10 15:50:02 +0900

    feat: 97

---
+++ example-app/app/Http/Controllers/Tweet/DeleteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tweet;
+use Illuminate\Http\Request;
+
+class DeleteController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $tweetId = (int)$request->route('tweetId');
+        Tweet::destroy($tweetId);
+
+        return redirect()
+            ->route('tweet.index')
+            ->with('feedback.success', 'つぶやきを削除しました');
+    }
+}
