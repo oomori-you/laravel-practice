da4bc2d891aa09f8dd5755e7ed19e74409b36b09
oomori-you <valsamina@gmail.com>
2024-06-13 16:13:39 +0900

    feat: 227

--- example-app/database/seeders/TweetsSeeder.php
+++ example-app/database/seeders/TweetsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Image;
 use App\Models\Tweet;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,10 @@ class TweetsSeeder extends Seeder
      */
     public function run(): void
     {
-        Tweet::Factory()->count(10)->create();
+        Tweet::Factory()->count(10)->create()->each(fn($tweet) =>
+            Image::factory()->count(4)->create()->each(fn($image) =>
+                $tweet->images()->attach($image->id)
+            )
+        );
     }
 }
